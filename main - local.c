e41f9fdf8a98017102eb046f347fb6d13dcb4438
christina <christina.oh.y@gmail.com>
2021-10-22 04:25:41 -0700

    added local main.c file for git purposes

---
+++ main - local.c
@@ -0,0 +1,17 @@
+/*
+Name: Christina Oh
+Date: 10/22/2021
+Project: Assignment 3
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+
+
+int main() {
+	// test
+}

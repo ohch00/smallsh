a095f4a7adf87dfcc8d5d7ac3402578f613aee57
christina <christina.oh.y@gmail.com>
2021-10-22 06:50:11 -0700

    parsing user_input string to get args

--- main - local.c
+++ main - local.c
@@ -6,33 +6,43 @@ Project: Assignment 3
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdbool.h>
 
 // Prompt, Comments & Blank Lines
 void process_user_input(char* user_input) {
-	char args[512][2048];
+	char args[512][2049];
+	char copy_user_input[2049];
+	strcpy(copy_user_input, user_input);
 
-	char* token = strtok_r(user_input, " ");
+	// remove /n character at input
+	copy_user_input[strlen(copy_user_input) - 1] = 0;
+
+	char* token = strtok(copy_user_input, " ");
 	char* saveptr = calloc(strlen(token) + 1, sizeof(char));
 
-	int counter = 1;
+	int counter = 0;
 	bool special_symbol = false;
 	char ampersand = '&';
 	char lesser_than = '<';
 	char greater_than = '>';
+	int conversion;
 
-	while (token != NULL && not special_symbol) {
+	while (token != NULL && !special_symbol) {
 		strcpy(saveptr, token);
-		if (strcmp(saveptr, ampersand) == 0 || strcmp(saveptr, lesser_than) == 0 || strcmp(saveptr, greater_than) == 0) {
+		conversion = saveptr[0];
+		if (conversion == ampersand || conversion == lesser_than || conversion == greater_than) {
 			special_symbol = true;
 			break;
 		}
+
 		token = strtok(NULL, " ");
 		strcpy(args[counter], saveptr);
 		counter = counter + 1;
 	}
+
 	free(saveptr);
 
 	if (special_symbol) {
